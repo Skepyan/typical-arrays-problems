893991ea9619a96bde8cfb6918e66355fd6ed559
unknown <you@example.com>
2021-02-08 22:04:14 +0300

    function min and max done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
+const { sort } = require("semver");
 
 exports.min = function min (array) {
-  return 0;
+let result = 0;
+
+if (array){
+  const sortArray = array.sort(function(a, b){
+    if(a < b ){
+      return -1;
+    }
+    if (a > b){
+      return 1;
+    }
+    if (a === b){
+      return 0;
+    }
+  });
+
+  result = sortArray[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+
+  return result;
 }
+exports.max = function max (array) {
+  let result = 0;
+
+  if (array){
+    const sortArray = array.sort(function(a, b){
+      if(a < b ){
+        return -1;
+      }
+      if (a > b){
+        return 1;
+      }
+      if (a === b){
+        return 0;
+      }
+    });
+  
+    result = sortArray[sortArray.length -1];
+  }
+  
+  
+    return result;
+};
 
 exports.avg = function avg (array) {
   return 0;
